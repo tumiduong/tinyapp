1c3e854993c6e87ea8fe5936adbe6a70e2a73787
tumiduong <duong.julie@live.com>
2019-09-03 00:14:03 +0000

    added partials and separate tinyurl page

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8080;
+
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -11,8 +13,14 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get("/urls", (req, res) => {
+  let templateVar = { urls: urlDatabase }
+  res.render("urls_index", templateVar);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVar = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVar);
 });
 
 app.get("/hello", (req, res) => {
